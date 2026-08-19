d014f73dddf85abe244f24a6a66818723b1f7ff2
Shobhit Kumar <shobhitkumar1437@gmail.com>
2026-08-20 00:27:53 +0530

    add ads.txt file for Google AdSense authorization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,6 +87,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="google-adsense-account" content="ca-pub-2480475355692793" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
